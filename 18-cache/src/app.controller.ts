c980633d43f112e72147617c813a08550876e32b
waiphyo285 <waiphyoenaing.joy007@gmail.com>
2022-07-09 15:59:40 +0630

    update cache demo

---
+++ 18-cache/src/app.controller.ts
@@ -0,0 +1,21 @@
+import {
+  CacheInterceptor,
+  Controller,
+  Get,
+  UseInterceptors,
+} from '@nestjs/common';
+import { HttpCacheInterceptor } from './common/http-cache.interceptor';
+
+@Controller()
+@UseInterceptors(HttpCacheInterceptor)
+export class AppController {
+  @Get()
+  findAll() {
+    return [
+      {
+        id: 1,
+        name: 'Nest'
+      }
+    ];
+  }
+}
---
+++ 18-cache/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module, CacheModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [CacheModule.register()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule { }
